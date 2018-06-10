1ec6d0c6255cc37403c000bbf673c7422836f997
NicolaIomm <alocinalocin@gmail.com>
2018-06-10 15:51:59 +0200

    Partial sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+  	@user = User.find(params[:id])
+  	#debugger => This stops the rails terminal to examine the situation, to continue press Ctrl+D
   end
+  
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		#Handle a successfully save
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	end
+
 end
